8265cb5912d07d2507d81fd02c9a96fbaed85494
olegpascal-front <olegpascal38@gmail.com>
2025-07-29 23:52:39 +0500

    fix: update project configuration for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import gltf from 'vite-plugin-gltf'; // Импортируем плагин для GLTF
+import gltf from 'vite-plugin-gltf';
+import glsl from 'vite-plugin-glsl';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    gltf(), // Добавляем плагин для GLTF
+    gltf(),
+    glsl()
   ],
+  optimizeDeps: {
+    exclude: ['@rollup/rollup-linux-x64-gnu']
+  },
+  build: {
+    target: 'es2020',
+    chunkSizeWarningLimit: 1600
+  }
 });
